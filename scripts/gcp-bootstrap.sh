2caa72295cb6be1788f26e96577e63eee7b5c43f
juneon <juneon0830@naver.com>
2026-05-23 23:44:35 +0900

    Cloud Run 시크릿 권한 설정

--- scripts/gcp-bootstrap.sh
+++ scripts/gcp-bootstrap.sh
@@ -49,6 +49,12 @@ for role in \
 done
 
 PROJECT_NUMBER="$(gcloud projects describe "${PROJECT_ID}" --format "value(projectNumber)")"
+RUNTIME_SERVICE_ACCOUNT="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
+
+gcloud projects add-iam-policy-binding "${PROJECT_ID}" \
+  --member "serviceAccount:${RUNTIME_SERVICE_ACCOUNT}" \
+  --role roles/secretmanager.secretAccessor \
+  --quiet
 
 if ! gcloud iam workload-identity-pools describe "${WORKLOAD_IDENTITY_POOL}" \
   --project "${PROJECT_ID}" \
